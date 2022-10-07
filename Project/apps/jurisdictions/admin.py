5e615a7c326716f3a6b6b4bf6f2caa65e79876ce
Charles Ngari <ngangaricharles@gmail.com>
2022-10-07 13:51:57 +0300

    jurisdictions

--- Project/apps/jurisdictions/admin.py
+++ Project/apps/jurisdictions/admin.py
@@ -1,3 +1,164 @@
-from django.contrib import admin
+import sys
 
-# Register your models here.
+from django.contrib import admin
+from django.db.models import Q
+from import_export.admin import ImportExportActionModelAdmin, ExportActionModelAdmin
+
+from apps.jurisdictions.models import Country, Region, County, SubCounty, Zone, Rank
+from apps.jurisdictions.resources import CountryResource, RegionResource, CountyResource, SubCountyResource, \
+    ZoneResource, RankResource
+from apps.core.project_requirements.admins import auto_admin_site
+from apps.core.project_requirements.utilities import Perm, Permi, active
+
+
+@admin.register(Country)
+class CountryAdmin(Perm, ImportExportActionModelAdmin):
+    list_per_page = sys.maxsize
+    resource_class = CountryResource
+    show_full_result_count = False
+    list_display = ('name', 'code')
+    search_fields = ('name', 'code')
+    list_display_links = ('name',)
+
+
+@admin.register(Region)
+class RegionAdmin(Perm, ImportExportActionModelAdmin):
+    list_per_page = sys.maxsize
+    resource_class = RegionResource
+    show_full_result_count = False
+    list_display = ('name', 'country')
+    list_filter = ('country',)
+    search_fields = ('name',)
+    autocomplete_fields = ['country']
+    list_select_related = ('country',)
+
+
+@admin.register(County)
+class CountyAdmin(Perm, ImportExportActionModelAdmin):
+    # actions = ExportActionModelAdmin.actions + [active]
+    list_per_page = sys.maxsize
+    resource_class = CountyResource
+    show_full_result_count = False
+    list_display = ('name', 'code', 'region')
+    list_filter = ('active', 'region',)
+    search_fields = ('name', 'code')
+    autocomplete_fields = ['region', ]
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.select_related('region').filter(active=True)
+
+    def get_actions(self, request):
+        actions = super().get_actions(request)
+        if not request.user.has_perm('jurisdictions.can_verify_county') and 'active' in actions:
+            del actions['active']
+        return actions
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        if not request.user.has_perm('jurisdictions.can_verify_county') and 'active' in form.base_fields:
+            form.base_fields['active'].disabled = True
+        return form
+
+
+@admin.register(SubCounty)
+class SubCountyAdmin(Perm, ImportExportActionModelAdmin):
+    # actions = ExportActionModelAdmin.actions + [active]
+    list_per_page = sys.maxsize
+    resource_class = SubCountyResource
+    show_full_result_count = False
+    list_display = ('name', 'county')
+    list_filter = ('active', 'county__region', 'county',)
+    search_fields = ('county__name', 'name', 'county__code')
+    autocomplete_fields = ['county', ]
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.select_related('county').filter(Q(county__active=True) & Q(active=True))
+
+    def get_actions(self, request):
+        actions = super().get_actions(request)
+        if not request.user.has_perm('jurisdictions.can_verify_sub_county') and 'active' in actions:
+            del actions['active']
+        return actions
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        if not request.user.has_perm('jurisdictions.can_verify_sub_county') and 'active' in form.base_fields:
+            form.base_fields['active'].disabled = True
+        return form
+
+
+@admin.register(Zone)
+class ZoneAdmin(Perm, ImportExportActionModelAdmin):
+    list_per_page = sys.maxsize
+    resource_class = ZoneResource
+    show_full_result_count = False
+    list_display = ('name', 'sub_county')
+    list_filter = ('sub_county__county', 'sub_county',)
+    search_fields = ('sub_county__county__name', 'sub_county__name', 'name')
+    autocomplete_fields = ['sub_county', ]
+
+
+@admin.register(Rank)
+class RankAdmin(Perm, ImportExportActionModelAdmin):
+    # resource_class = RankResource
+    list_display = ('code', 'level', 'name')
+    search_fields = ('code', 'name', 'level')
+    list_filter = ('level',)
+
+
+class AutoCountryAdmin(Perm, Permi, admin.ModelAdmin):
+    list_per_page = sys.maxsize
+    show_full_result_count = False
+    search_fields = ('name', 'code')
+
+
+class AutoRegionAdmin(Perm, Permi, admin.ModelAdmin):
+    list_per_page = sys.maxsize
+    show_full_result_count = False
+    search_fields = ('name',)
+
+
+class AutoCountyAdmin(Perm, Permi, admin.ModelAdmin):
+    list_per_page = sys.maxsize
+    show_full_result_count = False
+    search_fields = ('name', 'code')
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.select_related('region').filter(active=True)
+
+
+class AutoSubCountyAdmin(Perm, Permi, admin.ModelAdmin):
+    list_per_page = sys.maxsize
+    show_full_result_count = False
+    search_fields = ('county__name', 'name', 'county__code')
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.select_related('county').filter(Q(county__active=True) & Q(active=True))
+
+
+class AutoZoneAdmin(Perm, Permi, admin.ModelAdmin):
+    list_per_page = sys.maxsize
+    show_full_result_count = False
+    search_fields = ('sub_county__county__name', 'sub_county__name', 'name')
+
+
+class AutoRankAdmin(Perm, Permi, admin.ModelAdmin):
+    list_per_page = sys.maxsize
+    show_full_result_count = False
+    search_fields = ('code', 'name', 'level')
+
+
+auto_admin_site.register(Country, AutoCountryAdmin)
+auto_admin_site.register(Region, AutoRegionAdmin)
+auto_admin_site.register(County, AutoCountyAdmin)
+auto_admin_site.register(SubCounty, AutoSubCountyAdmin)
+auto_admin_site.register(Zone, AutoZoneAdmin)
+auto_admin_site.register(Rank, AutoRankAdmin)
--- Project/apps/jurisdictions/models.py
+++ Project/apps/jurisdictions/models.py
@@ -1,3 +1,125 @@
 from django.db import models
+from django.db.models import UniqueConstraint
+from django.urls import reverse
 
-# Create your models here.
+from apps.jurisdictions.managers import CountryManager, RegionManager, CountyManager, SubCountyManager, ZoneManager, \
+    RankManager
+from django.utils.translation import gettext_lazy as _
+
+
+class Country(models.Model):
+    code = models.CharField(_('code'), primary_key=True, max_length=2, db_index=True)
+    name = models.CharField(_('name'), max_length=50, unique=True, db_index=True)
+
+    objects = CountryManager()
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(fields=['code', 'name'], name='unique_country')
+        ]
+        verbose_name = _('country')
+        verbose_name_plural = _('countries')
+
+    def __str__(self):
+        return self.name
+
+
+class Region(models.Model):
+    name = models.CharField(_('name'), max_length=50, primary_key=True, db_index=True)
+    country = models.ForeignKey(Country, on_delete=models.PROTECT, db_index=True)
+
+    objects = RegionManager()
+
+    class Meta:
+        verbose_name = _('region')
+        verbose_name_plural = _('regions')
+
+    def __str__(self):
+        return self.name
+
+
+class County(models.Model):
+    code = models.PositiveSmallIntegerField(_('code'), primary_key=True, db_index=True)
+    name = models.CharField(_('name'), max_length=50, unique=True)
+    region = models.ForeignKey(Region, on_delete=models.PROTECT, db_index=True)
+    active = models.BooleanField(_('activeness'), default=False, db_index=True)
+
+    objects = CountyManager()
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(fields=['code', 'name'], name='unique_county')
+        ]
+        permissions = [('can_verify_county', 'Can verify Counties')]
+        verbose_name = _('county')
+        verbose_name_plural = _('counties')
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def reg_amount(self):
+        return 1000
+
+
+class SubCounty(models.Model):
+    name = models.CharField(_('name'), primary_key=True, max_length=50)
+    county = models.ForeignKey(County, on_delete=models.PROTECT, db_index=True)
+    active = models.BooleanField(_('activeness'), default=False, db_index=True)
+
+    objects = SubCountyManager()
+
+    class Meta:
+        permissions = [('can_verify_sub_county', 'Can verify SubCounty')]
+        verbose_name = _('sub_county')
+        verbose_name_plural = _('sub_counties')
+
+    def __str__(self):
+        return f'{self.county}/{self.name}'
+
+    @property
+    def reg_amount(self):
+        return 500
+
+
+class Zone(models.Model):
+    name = models.CharField(_('name'), primary_key=True, max_length=50)
+    sub_county = models.ForeignKey(SubCounty, on_delete=models.PROTECT, db_index=True)
+
+    objects = ZoneManager()
+
+    class Meta:
+        verbose_name = _('zone')
+        verbose_name_plural = _('zones')
+
+    def __str__(self):
+        return f'{self.sub_county}/{self.name}'
+
+
+class Rank(models.Model):
+    RANK_LEVEL = (
+        ('National', _('National Level')),
+        ('Regional', _('Regional Level')),
+        ('County', _('County Level')),
+        ('SubCounty', _('SubCounty Level')),
+        ('Zonal', _('Zonal Level')),
+        ('Unit', _('Unit Level')),
+        ('Scouts', _('Scouts Level')),
+    )
+    code = models.CharField(_('code'), primary_key=True, max_length=20)
+    name = models.CharField(_('name'), max_length=100, unique=True)
+    level = models.CharField(_('level'), max_length=20, choices=RANK_LEVEL, db_index=True)
+
+    objects = RankManager()
+
+    class Meta:
+        unique_together = ['code', 'name']
+        ordering = ['level']
+        verbose_name = _('rank')
+        verbose_name_plural = _('ranks')
+
+    def __str__(self):
+        return f'{self.code} - ({self.get_level_display()})'
+
+    def get_absolute_url(self):
+        return reverse('ranks:r_detail', kwargs={'pk': self.pk})
