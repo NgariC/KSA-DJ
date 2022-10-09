a12e81130a93033613f17b94194efc1fef429e05
Charles Ngari <ngangaricharles@gmail.com>
2022-10-09 09:35:57 +0300

    main urls

--- Project/Project/urls.py
+++ Project/Project/urls.py
@@ -4,12 +4,26 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include, re_path
 
+from apps.core.project_requirements.admins import auto_admin_site, stats_admin_site, strict_admin_site
+
+admin.site.site_header = "KSA Portal Administration"
+admin.site.site_title = "KSA Administrator"
+admin.site.index_title = "KSA Administrator's Portal"
+admin.site.empty_value_display = "-----"
+admin.site.enable_nav_sidebar = False
+# admin.site.save_as = True
+
 urlpatterns = [
     path('i18n/', include('django.conf.urls.i18n')),
+    path('mhgfhjkghjksiuruyfdmbjkdsfghjgkajkljdfhjgyuegfuygjsdbcjhguds', auto_admin_site.urls),
 ]
 
 urlpatterns += i18n_patterns(
-    path('admin/', admin.site.urls),
+    path('strict/doc/', include('django.contrib.admindocs.urls')),
+    path('ksa-hq/', admin.site.urls),
+    path('stats/', stats_admin_site.urls),
+    path('strict/', strict_admin_site.urls),
+    path('tinymce/', include('tinymce.urls')),
 )
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
@@ -20,4 +34,4 @@ if settings.DEBUG:
     urlpatterns = [path('__debug__/', include(debug_toolbar.urls)), ] + urlpatterns
 
 if 'rosetta' in settings.INSTALLED_APPS:
-    urlpatterns += [re_path('rosetta/', include('rosetta.urls'))]
+    urlpatterns += [re_path('translations/', include('rosetta.urls'))]
